4246e9942390dbb6fd8424d6625ca8dff379b52e
Thiago Santos <thiago.santos@pia.com.br>
2016-09-13 11:01:35 -0300

    Remoção do Login Local, movido para Login Global, criação da pasta de 'Includes'

--- index.php
+++ index.php
@@ -2,6 +2,21 @@
 
 session_start();
 
-if(empty($_SESSION['usuario'])) header("location: ./login.php");
+if(empty($_SESSION['usuario'])) header("location: ../login.php");
 
-?>
+else {
+
+// Incluindo Cabeçalho Padrão
+include "includes/cabecalho.html";
+
+// Incluindo Corpo
+include "includes/conteudo.html";
+
+// Incluindo Scripts
+include "includes/scripts.html";
+
+// Incluindo rodapé Padrão
+include "includes/rodape.html";
+
+}
+
