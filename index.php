47ee2256d1209c97370cf26f86895d17c465b24d
Thiago Santos <thiago.santos@pia.com.br>
2016-09-14 08:35:51 -0300

    Inseridos comandos SQlDL

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 session_start();
 
+ini_set('display_errors',1);
+ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
+
 if(!empty($_SESSION['usuario'])) header("location: ../login.php");
 
 else {
--- js/navegacao.js
+++ js/navegacao.js
@@ -2,6 +2,13 @@
 
 $(document).ready(function() {
 	$('.acao').click(function(e) {
+		var elementos = document.getElementsByClassName('acao');
+		//alert(elementos);
+		for (var x = 0; x < elementos.length; x++) {
+			elementos[x].className = "acao";
+		}
+
+		this.className = "acao selecionado";
 		e.preventDefault();
 		var exibir = this.href.split('#');
 		//alert(exibir[1]);
