83138bc47318bd79e9694f0e079599548ab1053b
Thiago Santos <thiago.santos@pia.com.br>
2016-09-14 09:12:10 -0300

    Scripts Atualizados e Comentados

--- js/navegacao.js
+++ js/navegacao.js
@@ -3,17 +3,16 @@
 $(document).ready(function() {
 	$('.acao').click(function(e) {
 		var elementos = document.getElementsByClassName('acao');
-		//alert(elementos);
+
 		for (var x = 0; x < elementos.length; x++) {
 			elementos[x].className = "acao";
 		}
 
 		this.className = "acao selecionado";
-		e.preventDefault();
-		var exibir = this.href.split('#');
-		//alert(exibir[1]);
+		e.preventDefault(); // Previne o Comportamento padrão do Link
+		var exibir = this.href.split('#'); // Recebe a Página para Navegação
 
-		$('section').hide();
-		$('#'+exibir[1]).fadeIn(700);
+		$('section').hide(); // Esconde Páginas
+		$('#'+exibir[1]).fadeIn(700); // Exibe Página Selecionada
 	});
 });
