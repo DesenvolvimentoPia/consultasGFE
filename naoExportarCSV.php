7fca52c263dc69b59c314390fce7a54e6d5d4e35
Thiago Santos <thgsantos16@gmail.com>
2016-09-22 11:38:05 -0300

    Versão Beta 1.0.1

---
+++ naoExportarCSV.php
@@ -0,0 +1,35 @@
+<?php
+// output headers so that the file is downloaded rather than displayed
+//header('Content-Type: text/csv; charset=utf-8');
+//header('Content-Disposition: attachment; filename='.date("Y-m-d-H-i-s").'.csv');
+
+// create a file pointer connected to the output stream
+$output = fopen('php://output', 'w');
+
+ini_set("soap.wsdl_cache_enabled", "0");
+ini_set("default_socket_timeout", "120");
+
+ini_set('display_errors',1);
+ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
+ 
+
+// A seguir você devera informar a URL do webservice.
+$oSoapClient = new SoapClient("http://lanpiasoa/wsdl/server/readFretesGFEService.wsdl");
+ 
+$aOptions = array (
+       "start_debug"=> "1",
+       "debug_port"=> "10000",
+       "debug_host"=> "localhost",
+       "debug_stop"=> "1",
+       "trace"=> "1");
+ 
+foreach($aOptions as $key => $val) {
+        $oSoapClient->__setCookie($key,$val);
+}
+
+//$resposta = $oSoapClient->readTicketSuporteFuncao("28", "a", "a", "a", "a", "a", "a", 'thiago.santos', 'tpiasl2k16*');
+$resposta = $oSoapClient->readFretesGFEFuncao($_GET['inicio'], $_GET['final'], $_GET['metodo'], 'thiago.santos', 'tpiasl2k16*');
+
+$formatted_array = array_map(function($num){ if(is_double($num)) return number_format($num,2); else return $num; }, $resposta);
+print_r($formatted_array);
